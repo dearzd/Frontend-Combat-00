dcd42d76c800e48de52ed970b6a88608f65ec833
dearzd <1147433303@qq.com>
2021-09-15 22:58:48 +0800

    week10 homework: el-icon

---
+++ week10/el-icon/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <Icon :size="48" color="violet">
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1024 1024"
+    >
+      <path
+          d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"
+      ></path>
+    </svg>
+  </Icon>
+</template>
+
+<script>
+import Icon from './Icon';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: { Icon }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ week10/el-icon/src/components/Icon.vue
@@ -0,0 +1,44 @@
+<template>
+  <i class="el-icon" :style="style" v-bind="$attrs">
+    <slot></slot>
+  </i>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+
+export default defineComponent({
+  name: 'Icon',
+  props: {
+    size: {
+      type: Number,
+    },
+    color: {
+      type: String,
+    }
+  },
+  setup(props) {
+    return {
+      style: computed(() => {
+        if (!props.size && !props.color) {
+          return {}
+        }
+
+        return {
+          'font-size': props.size ? `${props.size}px` : undefined,
+          'color': props.color || undefined
+        };
+      })
+    };
+  }
+})
+</script>
+
+
+<style lang="css">
+.el-icon svg {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+}
+</style>
